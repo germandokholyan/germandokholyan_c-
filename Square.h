2000b745ee3786b32ba8a8096c7e1eedef37214f
germandokholyan <gerahautani@gmail.com>
2025-04-07 11:34:23 +0300

    Update Square.h

--- Square.h
+++ Square.h
@@ -7,11 +7,14 @@ private:
     Point point1;
     Point point2;
     Point point3;
+    Point point4; 
     double sideLength;
     void validateSquare();
     double calculateDistance(const Point& p1, const Point& p2) const;
+    Point calculateFourthPoint() const; 
 public:
     Square(const Point& p1, const Point& p2, const Point& p3);
     double getArea() const;
     double getPerimeter() const;
+    bool containsPoint(const Point& p) const;
 };
