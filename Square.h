b2f4d6943e7c7df587e21dd5eca79e701e8f80b8
germandokholyan <gerahautani@gmail.com>
2025-05-02 15:00:57 +0300

    Update Square.h

--- Square.h
+++ Square.h
@@ -9,8 +9,22 @@ private:
     Point point3;
     Point point4; 
     double sideLength;
+    /**
+     * @brief Проверяет, что точки образуют квадрат
+     * @throws std::invalid_argument Если точки не образуют квадрат или совпадают
+     */
     void validateSquare() const;
+    /**
+     * @brief Вычисляет расстояние между двумя точками
+     * @param p1 Первая точка
+     * @param p2 Вторая точка
+     * @return Расстояние между точками
+     */
     double calculateDistance(const Point& p1, const Point& p2) const;
+    /**
+     * @brief Вычисляет четвертую точку квадрата по трем заданным
+     * @return Четвертая точка квадрата
+     */
     Point calculateFourthPoint() const; 
 public:
     Square(const Point& p1, const Point& p2, const Point& p3);
