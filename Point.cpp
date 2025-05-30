f27b603c1a3d32afe54f9946d25142b298894abf
germandokholyan <gerahautani@gmail.com>
2025-05-30 20:33:55 +0300

    Update Point.cpp

--- Point.cpp
+++ Point.cpp
@@ -30,6 +30,9 @@ bool Point::operator==(const Point& other) const {
     return x == other.x && y == other.y;
 }
 
+bool Point::operator!=(const Point& other) const {
+    return !(*this == other);  
+}
 std::ostream& operator<<(std::ostream& os, const Point& p) {
     os << "(" << p.x << "," << p.y << ")";
     return os;
