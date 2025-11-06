9c15152b0e3ebd1663969d507401fe834f71c730
germandokholyan <gerahautani@gmail.com>
2025-11-06 10:42:19 +0300

    Create IStreamGenerator.h

---
+++ IStreamGenerator.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "Generator.h"
+#include <iostream>
+
+namespace miit::algebra
+{
+    class IStreamGenerator : public Generator
+    {
+    private:
+        std::istream& in;
+
+    public:
+        IStreamGenerator(std::istream& in = std::cin);
+        int generate() override;
+    };
+}
