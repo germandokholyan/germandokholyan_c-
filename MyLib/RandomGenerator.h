99cfbc96101617feb852f6778fed14feca6ab26b
germandokholyan <gerahautani@gmail.com>
2025-12-07 17:03:37 +0300

    Update RandomGenerator.h

--- MyLib/RandomGenerator.h
+++ MyLib/RandomGenerator.h
@@ -5,10 +5,11 @@ namespace miit::algebra {
     /**
      * @brief Генератор случайных чисел.
      */
-    class RandomGenerator : public Generator {
+    class RandomGenerator : public Generator<int> {
     private:
         std::uniform_int_distribution<int> distribution;
         std::mt19937 generator;
+
     public:
         /**
          * @brief Конструктор.
@@ -16,7 +17,6 @@ namespace miit::algebra {
          * @param max Максимальное значение.
          */
         RandomGenerator(const int min, const int max);
-
         /**
          * @brief Генерирует случайное число в заданном диапазоне.
          * @return Случайное целое число.
