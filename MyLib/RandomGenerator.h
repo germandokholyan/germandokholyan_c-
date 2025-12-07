e365b373e2a59875e77f45280d92ac577a1ad361
germandokholyan <gerahautani@gmail.com>
2025-12-07 16:52:20 +0300

    Create RandomGenerator.h

---
+++ MyLib/RandomGenerator.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "Generator.h"
+#include <random>
+namespace miit::algebra {
+    /**
+     * @brief Генератор случайных чисел.
+     */
+    class RandomGenerator : public Generator {
+    private:
+        std::uniform_int_distribution<int> distribution;
+        std::mt19937 generator;
+    public:
+        /**
+         * @brief Конструктор.
+         * @param min Минимальное значение.
+         * @param max Максимальное значение.
+         */
+        RandomGenerator(const int min, const int max);
+
+        /**
+         * @brief Генерирует случайное число в заданном диапазоне.
+         * @return Случайное целое число.
+         */
+        int generate() override;
+    };
+} // namespace miit::algebra
