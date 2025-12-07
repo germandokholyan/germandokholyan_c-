badc8ab1ad889e23b1dc87c79c98e3b4956310ac
germandokholyan <gerahautani@gmail.com>
2025-12-07 17:03:02 +0300

    Update RandomGenerator.cpp

--- MyLib/RandomGenerator.cpp
+++ MyLib/RandomGenerator.cpp
@@ -1,4 +1,4 @@
-  #include "RandomGenerator.h"
+#include "RandomGenerator.h"
 miit::algebra::RandomGenerator::RandomGenerator(const int min, const int max)
     : generator(std::mt19937(std::random_device{}()))
     , distribution(std::uniform_int_distribution<int>(min, max))
