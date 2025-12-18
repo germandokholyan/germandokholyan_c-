b74ae38e31e4b99f9f69496e352231ef6002a6c0
germandokholyan <gerahautani@gmail.com>
2025-12-18 12:04:19 +0300

    Update Ex1.cpp

--- Lab4_2/Ex1.cpp
+++ Lab4_2/Ex1.cpp
@@ -5,7 +5,7 @@ miit::algebra::Ex1::Ex1(int n, std::unique_ptr<Generator<int>> gen)
 }
 void miit::algebra::Ex1::Task1()
 {
-    for (int i = 0; i < matrix->getSize(); ++i) {
+    for (size_t i = 0; i < matrix->getSize(); ++i) {
         if ((*matrix)[i] < 0) {
             (*matrix)[i] = 0;
             break; // Заменяем только первый отрицательный элемент
