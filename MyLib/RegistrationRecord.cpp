8b6e560cb11ae6d137ace9f9cb786af4a0ba4a0c
germandokholyan <gerahautani@gmail.com>
2025-12-21 15:00:02 +0300

    Update RegistrationRecord.cpp

--- MyLib/RegistrationRecord.cpp
+++ MyLib/RegistrationRecord.cpp
@@ -1,41 +1,60 @@
 #include "RegistrationRecord.h"
-#include <sstream>
-RegistrationRecord::RegistrationRecord(const std::shared_ptr<Vehicle>& veh, const std::shared_ptr<Owner>& own, const std::string& date)
-    : vehicle(veh), owner(own), registrationDate(date), previousOwners(""), accidentHistory("") {}
-std::shared_ptr<Vehicle> RegistrationRecord::getVehicle() const {
-    return vehicle;
-}
-std::shared_ptr<Owner> RegistrationRecord::getOwner() const {
-    return owner;
-}
-std::string RegistrationRecord::getRegistrationDate() const {
-    return registrationDate;
-}
-std::string RegistrationRecord::getPreviousOwners() const {
-    return previousOwners;
-}
-std::string RegistrationRecord::getAccidentHistory() const {
-    return accidentHistory;
-}
-void RegistrationRecord::addPreviousOwner(const std::string& ownerName) {
-    if (!previousOwners.empty()) {
-        previousOwners += ", ";
+#include "Vehicle.h"
+#include "Owner.h"
+
+namespace miit::gai {
+    RegistrationRecord::RegistrationRecord(const std::shared_ptr<Vehicle>& veh,
+                                         const std::shared_ptr<Owner>& own,
+                                         const std::string& date)
+        : vehicle(veh), owner(own), registrationDate(date) {}
+
+    std::shared_ptr<Vehicle> RegistrationRecord::getVehicle() const {
+        return vehicle;
     }
-    previousOwners += ownerName;
-}
-void RegistrationRecord::addAccident(const std::string& accidentDetails) {
-    if (!accidentHistory.empty()) {
-        accidentHistory += "; ";
+    std::shared_ptr<Owner> RegistrationRecord::getOwner() const {
+        return owner;
     }
-    accidentHistory += accidentDetails;
-}
-std::string RegistrationRecord::getInfo() const {
-    std::ostringstream oss;
-    oss << "Запись регистрации:\n"
-        << "Дата регистрации: " << registrationDate << "\n"
-        << "Автомобиль: " << vehicle->getInfo() << "\n"
-        << "Владелец: " << owner->getInfo() << "\n"
-        << "Предыдущие владельцы: " << (previousOwners.empty() ? "Нет" : previousOwners) << "\n"
-        << "История аварий: " << (accidentHistory.empty() ? "Нет" : accidentHistory);
-    return oss.str();
-}
+
+    std::string RegistrationRecord::getRegistrationDate() const {
+        return registrationDate;
+    }
+
+    std::vector<std::string> RegistrationRecord::getPreviousOwners() const {
+        return previousOwners;
+    }
+
+    std::vector<std::string> RegistrationRecord::getAccidentHistory() const {
+        return accidentHistory;
+    }
+
+    void RegistrationRecord::addPreviousOwner(const std::string& ownerName) {
+        previousOwners.push_back(ownerName);
+    }
+
+    void RegistrationRecord::addAccident(const std::string& accidentDetails) {
+        accidentHistory.push_back(accidentDetails);
+    }
+
+    std::string RegistrationRecord::getInfo() const {
+        std::string info = "Регистрация: " + registrationDate + "\n";
+        info += "Автомобиль: " + vehicle->getBrand() + " " + vehicle->getModel() + "\n";
+        info += "Владелец: " + owner->getFullName() + "\n";
+
+        if (!previousOwners.empty()) {
+            info += "Предыдущие владельцы:\n";
+            for (const auto& name : previousOwners) {
+                info += "  - " + name + "\n";
+            }
+        }
+
+        if (!accidentHistory.empty()) {
+            info += "Аварии:\n";
+            for (const auto& details : accidentHistory) {
+                info += "  - " + details + "\n";
+            }
+        }
+
+        return info;
+    }
+
+} // namespace miit::gai
