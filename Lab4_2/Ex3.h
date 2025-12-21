0f5d9fef741461175f1b5ae1c47cd0b3e194c665
germandokholyan <gerahautani@gmail.com>
2025-12-21 14:35:45 +0300

    Update Ex3.h

--- Lab4_2/Ex3.h
+++ Lab4_2/Ex3.h
@@ -1,6 +1,8 @@
 #pragma once
 #include "Exercise.h"
+
 namespace miit::algebra {
+
     /**
      * @brief Класс, реализующий третий пункт задания (Из элементов массива D сформировать массив A той же размерности по правилу...).
      */
@@ -12,13 +14,11 @@ namespace miit::algebra {
          * @param gen Указатель на генератор значений.
          */
         Ex3(int n, std::unique_ptr<Generator<int>> gen);
-        /**
-         * @brief Выполняет задачу 1: не используется в этом пункте.
-         */
-        void Task1() override;
+
         /**
          * @brief Выполняет задачу 2: формирует массив A по правилу.
          */
-        void Task2() override;
+        void execute(); // Изменено: без параметров, так как правило не требует дополнительных данных
     };
+
 }
