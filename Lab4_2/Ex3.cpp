d7e2feee4f50bcd68e5b035d046d28898681b46c
germandokholyan <gerahautani@gmail.com>
2025-12-21 14:35:14 +0300

    Update Ex3.cpp

--- Lab4_2/Ex3.cpp
+++ Lab4_2/Ex3.cpp
@@ -6,9 +6,9 @@ namespace miit::algebra {
         // Формируем массив A по правилу: если номер четный, то A_i = (i-1)*D_i, иначе A_i = D_i * i^2
         for (size_t i = 0; i < static_cast<size_t>(matrix->getSize()); ++i) {
             if ((i + 1) % 2 == 0) { // Четный номер (i+1 - потому что номера начинаются с 1)
-                (*matrix)[i] = (static_cast<int>(i)) * (*matrix)[i]; // (i-1) в формуле -> i, так как i в цикле начинается с 0
+                (matrix)[i] = (static_cast<int>(i)) * (matrix)[i]; // (i-1) в формуле -> i, так как i в цикле начинается с 0
             } else {
-                (*matrix)[i] = (*matrix)[i] * (static_cast<int>(i) + 1) * (static_cast<int>(i) + 1); // i^2 -> (i+1)^2
+                (matrix)[i] = (matrix)[i] * (static_cast<int>(i) + 1) * (static_cast<int>(i) + 1); // i^2 -> (i+1)^2
             }
         }
     }
