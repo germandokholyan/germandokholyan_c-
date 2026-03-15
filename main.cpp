93537935d47adc0b9c873d0884bf3053721a855d
germandokholyan <gerahautani@gmail.com>
2026-03-15 11:09:26 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include "pt4.h"
+#include <list>
+#include <iterator>
+#include <algorithm>
+using namespace std;
+void Solve() {
+    Task("PTask2");
+    // Заполнение списка с использованием ptin_iterator
+    list<int> L{ ptin_iterator<int>(), ptin_iterator<int>() };
+    // Проверка, что количество элементов делится на 3
+    if (L.empty() || L.size() % 3 != 0) 
+    {
+        // Обработка ошибки или пропуск
+        return;
+    }
+    auto third = L.size() / 3;
+    // Первая треть в исходном порядке
+    auto it_end_first = L.begin();
+    advance(it_end_first, third);
+    copy(L.begin(), it_end_first, ptout_iterator<int>());
+    // Вторая треть в обратном порядке
+    auto it_end_second = L.begin();
+    advance(it_end_second, 2 * third);
+    auto it_first_second = L.begin();
+    advance(it_first_second, third);
+    copy(make_reverse_iterator(it_end_second), make_reverse_iterator(it_first_second), 
+         ptout_iterator<int>());
+    // Последняя треть в обратном порядке
+    copy(make_reverse_iterator(L.end()), make_reverse_iterator(it_end_second), 
+         ptout_iterator<int>());
+}
